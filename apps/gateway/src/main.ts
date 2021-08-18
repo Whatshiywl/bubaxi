b2f1d865c6f1b0101a987f70d710cd3e37374e50
Whatshiywl <whatshiywl@gmail.com>
2021-08-18 14:42:14 -0300

    Improve proxy configs

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -5,9 +5,8 @@
 
 import * as express from 'express';
 import { environment } from './environments/environment';
-import { createProxyMiddleware, Options } from 'http-proxy-middleware';
-import { readdirSync, readFileSync } from 'fs';
-import { join } from 'path';
+import proxy from './proxy';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const app = express();
 
@@ -15,52 +14,15 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to gateway!' });
 });
 
-// Setup proxies
-const proxiesDir = join(__dirname, 'proxy');
-const projects = readdirSync(proxiesDir);
-for (const project of projects) {
-  const proxyDir = join(proxiesDir, project);
-  const proxyConfs = readdirSync(proxyDir);
-  for (const conf of proxyConfs) {
-    const confFile = join(proxyDir, conf);
-    const proxyConfJson = readFileSync(confFile).toString();
-    const proxyConfs = JSON.parse(proxyConfJson) as { [route: string]: Options };
-    const routes = Object.keys(proxyConfs);
-    for (const route of routes) {
-      const proxyConf = proxyConfs[route];
-      let target = proxyConf.target as string;
-      const envMatch = target.match(/{([^}]+)}/g);
-      if (envMatch) {
-        for (const matched of envMatch) {
-          const key = matched.substring(1, matched.length - 1);
-          const value = process.env[key] || environment[key];
-          if (value) {
-            target = target.replace(matched, value);
-          }
-        }
-      }
-      const targetURL = new URL(target);
-      const basePath = targetURL.pathname;
-      const pathAppend = route === '/' ? '' : route;
-      const newRoute = join(basePath, pathAppend);
-      const newTarget = join(targetURL.origin, pathAppend);
-      const rewrite = `^${basePath}`;
-      const newConf = {
-        target: newTarget,
-        changeOrigin: proxyConf,
-        secure: proxyConf.secure,
-        router: proxyConf.router,
-        pathRewrite: { }
-      } as Options;
-      newConf.pathRewrite[rewrite] = '';
-      const proxy = createProxyMiddleware(newRoute, newConf);
-      app.use(proxy);
-    }
-  }
+const routes = Object.keys(proxy);
+for (const route of routes) {
+  const options = proxy[route];
+  const proxyMiddleware = createProxyMiddleware(route, options);
+  app.use(proxyMiddleware);
 }
 
 const port = process.env.PORT || environment.PORT || 3333;
 const server = app.listen(port, () => {
-  console.log(`Listening at port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
 server.on('error', console.error);
---
+++ apps/gateway/src/proxy.ts
@@ -0,0 +1,25 @@
+import { environment } from './environments/environment';
+import { Options } from "http-proxy-middleware";
+
+const logLevel = environment.production ? 'info' : 'debug';
+
+const proxy: { [path: string]: Options } = { };
+
+function addProxy(basePath: string, path: string, target: string, devTarget: string) {
+  const options: Options = {
+    target,
+    changeOrigin: true,
+    secure: false,
+    logLevel,
+    pathRewrite: { },
+    router: { 'localhost:3000': devTarget }
+  };
+  options.pathRewrite[`^${basePath}`] = ''
+  const fullPath = `${basePath}${path}`;
+  proxy[fullPath] = options;
+}
+
+addProxy('/test-npx', '/api', 'https://test-api-2bfaoux6cq-uc.a.run.app/', 'http://localhost:3080/');
+addProxy('/test-npx', '',     'https://test-npx-2bfaoux6cq-uc.a.run.app/', 'http://localhost:8080/');
+
+export default proxy;
