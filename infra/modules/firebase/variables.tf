7615efbe8d0e148d1821237ab30156a600bb2691
Whatshiywl <whatshiywl@gmail.com>
2025-07-09 14:57:58 -0300

    Deploy quintozap-ngx

--- infra/modules/firebase/variables.tf
+++ infra/modules/firebase/variables.tf
@@ -1,9 +1,13 @@
 variable "project_id"     { type = string }
 variable "app_name"       { type = string }
-variable "site_name"      { type = string }
 variable "build_dir"      { type = string }
+variable "site_name"      {
+  type = string
+  default = ""
+}
 
 locals {
+  site_name = var.site_name != "" ? var.site_name : var.app_name
   abs_build_dir = "${path.module}/../../../${var.build_dir}"
   assets_hash = sha1(join("", [
     for f in fileset(local.abs_build_dir, "**") :
@@ -13,7 +17,7 @@ locals {
   firebaserc_content = templatefile(local.firebaserc_template_path, {
     project_id = var.project_id
     app_name   = var.app_name
-    site_name  = var.site_name
+    site_name  = local.site_name
   })
   firebaserc_output_path = "${path.module}/../../../.firebaserc"
   firebase_json_template_path = "${path.module}/firebase.json.tftpl"
